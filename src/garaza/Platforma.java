8905ac011395e4955575e2831bdb8b728ebe88d7
Goran <Goran@lucky>
2018-12-16 11:08:28 +0100

    Kreirana Platforma sa svojim mjestima, Mjesta na platformi koja su parking mjesta su ozanacena sa atributom parkingMjesto iz klase MjestoNaPlatformi

--- src/garaza/Platforma.java
+++ src/garaza/Platforma.java
@@ -10,5 +10,40 @@ public class Platforma implements Serializable{
     
     public int brSlobodnihMjesta;
     public boolean popunjena;
+    public MjestoNaPlatformi[][] matrica;
+    
+    public Platforma(){
+        
+        matrica = new MjestoNaPlatformi[10][8];
+        for(int i=0;i<matrica.length;i++){
+            for(int j=0;j<matrica[i].length;j++){
+                matrica[i][j]=new MjestoNaPlatformi();
+                
+                if(i>1){
+                    if(j==0)
+                    matrica[i][j].parkingMjesto=true;
+                    if(j==7)
+                        matrica[i][j].parkingMjesto=true;
+                    if((i<10) && j==3)
+                        matrica[i][j].parkingMjesto=true;
+                    if((i<10) && j==4)
+                        matrica[i][j].parkingMjesto=true;
+                }
+            }            
+        }
+        
+        brSlobodnihMjesta = 28;
+        popunjena=false;
+    }
+    
+    public static void main(String [] args){
+        Platforma platforma = new Platforma();
+        
+        for(int i=0;i<platforma.matrica.length;i++){
+            for(int j=0;j<platforma.matrica[i].length;j++){
+                System.out.println("matrica["+i+"]["+j+"]: "+platforma.matrica[i][j]);
+            }
+        }
+    }
     
 }
